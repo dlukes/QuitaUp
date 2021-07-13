edb0dd48d95a9ebe6bef04b989352158fbab0a9e
David Lukes <dafydd.lukes@gmail.com>
2021-07-13 15:34:27 +0200

    Remove shinyCNC dependency

--- server.R
+++ server.R
@@ -2,7 +2,6 @@ library(shiny)
 library(shinyBS)
 library(tidyverse)
 library(shiny.i18n)
-library(shinyCNC)
 library(readtext)
 library(udpipe)
 library(stringr)
@@ -10,12 +9,9 @@ source("localized_ui.R")
 
 shinyServer(function(input, output, session) {
   
-  if(!init_shiny_cnc(appName))  { return() }
-  
-  lang <- get_lang(session)
+  lang <- "en"
   i18n <- Translator$new(translation_json_path = "data/translation.json")  
   i18n$set_translation_language(lang)
-  user <- get_user(session)
   
   output$localizedUI <- renderUI(localizedUI(i18n))
   
--- ui.R
+++ ui.R
@@ -1,13 +1,11 @@
 library(shiny)
 library(shiny.i18n)
 library(shinythemes)
-library(shinyCNC)
 
 shinyUI(
   bootstrapPage(
     tags$head( tags$link(rel = "stylesheet", type = "text/css", href = "calc.css?v=14") ),
     #shinybusy::add_busy_spinner(spin = "orbit", color = "#009ee0"),
-    shiny_cnc_UI(),
     #shinythemes::themeSelector(),
     theme = shinytheme("yeti"),
     uiOutput("localizedUI")
